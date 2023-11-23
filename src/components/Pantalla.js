1b9ce5970b33e42a3940f864694cdf0544abaa34
Evelin Alvarado <129849677+EvelinAlvarado@users.noreply.github.com>
2023-11-23 18:27:43 +0000

    Added Screen component for displaying calculator input

---
+++ src/components/Pantalla.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../stylesheets/Pantalla.css";
+
+// Componente con arrow function: para componentes mas sencillo (presentacion)
+// sintaxis de desestructuracion
+const Pantalla = ({ input }) => <div className="input">{input}</div>;
+
+export default Pantalla;
